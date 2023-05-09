3f450c263ee70775867de85718a8517e637d4e44
Daniel Leniz <danielmleniz@gmail.com>
2023-05-08 21:46:02 -0400

    terraform

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -1,17 +1,3 @@
-import json
-import boto3
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('crc')
-def lambda_handler(event, context):
-    response = table.get_item(Key={
-            'id':'0'
-    })
-    count = response['Item']['count']
-    count = count + 1
-    print(count)
-    response = table.put_item(Item={
-            'id':'0',
-            'count': count
-    })
-    
-    return count
+version https://git-lfs.github.com/spec/v1
+oid sha256:bd41e5146a2d136b62a75f3775f74bfe676747a53b10f5c89b306b4d0d34d7b4
+size 402
