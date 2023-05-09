f9e472f976a9f55e5f20cc38f5b71aa8a91e69ec
Daniel Leniz <danielmleniz@gmail.com>
2023-05-09 18:03:26 -0400

    added get method

--- backend/modules/tf-state/main.tf
+++ backend/modules/tf-state/main.tf
@@ -133,14 +133,6 @@ resource "aws_api_gateway_integration" "api-lambda-integration" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.view-counter-function.invoke_arn
 }
-resource "aws_api_gateway_integration" "api-lambda-integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-  resource_id             = aws_api_gateway_resource.api-resource.id
-  http_method             = aws_api_gateway_method.api-get-method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.view-counter-function.invoke_arn
-}
 
 # Deployment (to stage for use)
 resource "aws_api_gateway_deployment" "api-deployment" {
