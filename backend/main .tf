63c3a0ab7413c89cd86df8bc3cf2f56d0cbf5274
Daniel Leniz <danielmleniz@gmail.com>
2023-05-08 21:30:20 -0400

    terraform

--- backend/main .tf
+++ backend/main .tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "3.0.2"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "4.66.1"
     }
   }
 
