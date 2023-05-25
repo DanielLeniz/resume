2365d9f86d84de3c39d6278e2bcf7f606218c582
Mackenzie Gittinger <mackenziegittinger@Mackenzies-MacBook-Air-2.local>
2023-05-25 12:17:01 +0200

    terraform

--- backend/providers.tf
+++ backend/providers.tf
@@ -2,17 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.67.0"
+      version = ">=4.9.0"
     }
   }
 }
 
-module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.10.1"
-}
 provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = ["/Users/mackenziegittinger/.aws/config"]
-  shared_credentials_files = ["/Users/mackenziegittinger/.aws/credentials"]
+  profile = "dleniz"
+  region = "us-east-1"
 }
