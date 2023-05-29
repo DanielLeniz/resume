e8df3f27480d8b696b4b00fcef51a06c12a02684
danielleniz <danielmleniz@gmail.com>
2023-05-29 17:40:11 +0200

    fixed lambda function

--- backend/main.tf
+++ backend/main.tf
@@ -22,7 +22,7 @@ resource "aws_lambda_function" "myfunc" {
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
   function_name    = "myfunc"
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "func.handler"
+  handler          = "lambda_function.lambda_handler"
   runtime          = "python3.8"
 }
 
