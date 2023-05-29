fe967236e03b64962448b2612b51a5942849d174
danielleniz <danielmleniz@gmail.com>
2023-05-29 11:40:55 +0200

    changed api

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-  let response = await fetch("https://p2gv3ro832.execute-api.us-east-1.amazonaws.com/dev/get-resume");
+  let response = await fetch("https://tz30e61t47.execute-api.us-east-1.amazonaws.com/dev/get_resume");
   let data = await response.json();
   counter.innerHTML = `You are visitor number <span style='color:rgb(190, 78, 78)'> ${data}</span>`;
 }
