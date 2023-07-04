87f80a9a84fad9438c81125b6bfa6adfc6954d0d
danielleniz <danielmleniz@gmail.com>
2023-07-04 16:35:25 +0200

    gitignore

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-  let response = await fetch("https://tz30e61t47.execute-api.us-east-1.amazonaws.com/dev/get_resume");
-  let data = await response.json();
-  counter.innerHTML = `You are visitor number <span style='color:rgb(190, 78, 78)'> ${data}</span>`;
-}
-
-updateCounter();
