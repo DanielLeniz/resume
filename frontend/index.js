4302819e58226569a2237127062ff2a292dee3ab
Daniel Leniz <danielmleniz@gmail.com>
2023-09-30 17:56:09 -0400

    changed layout

--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-  let response = await fetch("https://p2gv3ro832.execute-api.us-east-1.amazonaws.com/dev/get-resume");
-  let data = await response.json();
-  counter.innerHTML = `You are visitor number <span style='color:rgb(190, 78, 78)'> ${data}</span>`;
-}
-
-updateCounter();
