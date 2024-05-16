c206e7093a43246365c5e9cf7c1cf13d8384ea0f
Daniel Leniz <danielmleniz@gmail.com>
2024-05-16 17:17:22 -0400

    changed projects

--- backend/main.tf
+++ backend/main.tf
@@ -21,13 +21,13 @@ resource "aws_lambda_function" "myfunc" {
   filename         = data.archive_file.zip_the_python_code.output_path
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
   function_name    = "myfunc"
-  role             = aws_iam_role.iam_for_lambda.arn
+  role             = aws_iam_role.lambda_iam.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.8"
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+resource "aws_iam_role" "lambda_iam" {
+  name = "lambda_iam"
 
   assume_role_policy = <<EOF
 {
@@ -78,7 +78,7 @@ resource "aws_iam_policy" "iam_policy_for_resume_project" {
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role = aws_iam_role.iam_for_lambda.name
+  role = aws_iam_role.lambda_iam.name
   policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
   
 }
