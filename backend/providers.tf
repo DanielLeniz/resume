0eeb0fd0dfa33a0286299b3daed6dcd3da220e17
Daniel Leniz <danielmleniz@gmail.com>
2024-05-16 17:11:41 -0400

    changed providers

--- backend/providers.tf
+++ backend/providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["C:\Users\Daniel\.aws\config"]
-  shared_credentials_files = ["C:\Users\Daniel\.aws\credentials"]
+  shared_config_files      = ["C:/Users/Daniel/.aws/config"]
+  shared_credentials_files = ["C:/Users/Daniel/.aws/credentials"]
 }
